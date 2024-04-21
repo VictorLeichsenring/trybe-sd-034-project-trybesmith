0b81a83a24c4747cd27f004da40d1170a6326856
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-04-21 17:48:06 -0300

    commit para publicação

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,3 +1,4 @@
+import Joi from 'joi';
 import ProductModel, { 
   ProductInputtableTypes,
   ProductSequelizeModel,
@@ -5,7 +6,18 @@ import ProductModel, {
 import { Product } from '../types/Product';
 import { ServiceResponse } from '../types/ServiceResponse';
 
+const productSchema = Joi.object({
+  name: Joi.string().required().min(3),
+  userId: Joi.number().required(),
+  price: Joi.string().required().min(3),
+}); 
+
 async function create(product: ProductInputtableTypes): Promise<ServiceResponse<Product>> {
+  const { error } = productSchema.validate(product);
+  if (error) {
+    const errorMessage = error.details[0].message;
+    return { status: 'INVALID_DATA', data: { message: errorMessage } };
+  }
   const newProduct = await ProductModel.create(product);
   const responseService: ServiceResponse<Product> = { 
     status: 'CREATED', data: newProduct.dataValues };
--- src/utils/mapStatusHTTP.ts
+++ src/utils/mapStatusHTTP.ts
@@ -1,6 +1,6 @@
 export default function mapStatusHTTP(status: string): number {
   const statusHTTPMap: Record<string, number> = {
-    INVALID_DATA: 400,
+    INVALID_DATA: 422,
     UNAUTHORIZED: 401,
     NOT_FOUND: 404,
     SUCCESSFUL: 200,
